043c4f63d35d391cf299268319881b4333abd1a3
syedmubashiralii <81847925+syedmubashiralii@users.noreply.github.com>
2022-05-23 14:45:48 +0500

    Add files via upload

--- lib/controllers/utilites.dart
+++ lib/controllers/utilites.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_mj/models/doctormodel.dart';
+import 'package:flutter_application_mj/models/picturesmodel.dart';
 
 class Utilities {
   static Size getSize(BuildContext context) {
@@ -9,10 +10,17 @@ class Utilities {
   static bool isloading = false;
   static List<dynamic> data = [];
   static var baseurl = "http://192.168.43.24";
-  static int caretakerid=0;
+  static String dfeedback = "";
+  static String dfeedbackdes = "";
+  static int caretakerid = 0;
   static int doctorid = 0;
+  static int pictureid = 0;
   static int patientid = 0;
   static String audiopath = "";
   static String imgpath = "";
   static List<DoctorModel> doctorlist = [];
+  static List<Picturesmodel> pictureslist = [];
+  static String getstarted = "";
+  static int id = 0;
+  static bool state = false;
 }
